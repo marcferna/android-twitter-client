c8dfa6c46825f041aa044e60bf3d1c840d500e5c
Marc Fernandez <marc.fernandezg@gmail.com>
2014-09-25 22:18:20 -0700

    Added name

--- app/src/main/java/com/codepath/apps/twitterclient/TweetArrayAdapter.java
+++ app/src/main/java/com/codepath/apps/twitterclient/TweetArrayAdapter.java
@@ -36,11 +36,13 @@ public class TweetArrayAdapter extends ArrayAdapter<Tweet> {
     ImageView ivProfileImage = (ImageView) v.findViewById(R.id.ivProfileImage);
     TextView tvUserName = (TextView) v.findViewById(R.id.tvUserName);
     TextView tvBody = (TextView) v.findViewById(R.id.tvBody);
+    TextView tvName = (TextView) v.findViewById(R.id.tvName);
     TextView tvCreatedAt = (TextView) v.findViewById(R.id.tvCreatedAt);
 
     ivProfileImage.setImageResource(0);
     ImageLoader imageLoader = ImageLoader.getInstance();
     imageLoader.displayImage(tweet.getUser().getProfileImageUrl(), ivProfileImage);
+    tvName.setText(tweet.getUser().getName());
     tvUserName.setText(tweet.getUser().getScreenName());
     tvBody.setText(tweet.getBody());
     tvCreatedAt.setText(tweet.getRelativeCreatedAt());
